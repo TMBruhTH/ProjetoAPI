3b7d899d16a81025e429540bfe4fe7d9609c8272
TMBruhTH <brunno_1211@hotmail.com>
2022-04-30 18:38:41 -0300

    Add project files.

---
+++ ProjetoAPI/Data/ContatoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAPI.Data
+{
+    public class ContatoContext : DbContext
+    {
+        public DbSet<Contato> Contatos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ProjetoAPIContato;Data Source=.\BRUNOSQL");
+        }
+    }
+}
---
+++ ProjetoAPI/Model/Contato.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAPI.Model
+{
+    public class Contato
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateTime data_nascimento { get; set; }
+        public string cpf { get; set; }
+        public string rg { get; set; }
+        public string endereco { get; set; }
+        public DateTime data_cadastro { get; set; }
+        public bool ativo { get; set; }
+    }
+}
